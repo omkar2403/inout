dde9dcb07a9afebe25dd347e0ba6c7374b9ff83b
Omkar <omkar.kakeru@gmail.com>
2019-09-18 17:27:09 +0530

    [Solved] Query Issue in Login Page

--- login.php
+++ login.php
@@ -66,7 +66,7 @@
                         </div>
                        <select name="loc" required="" class="selectpicker" data-style="select-with-transition" title="Select Location">
                         <?php
-                          $query = "SELECT * FROM LOC";
+                          $query = "SELECT * FROM loc";
                           $res = mysqli_query($conn, $query) or die("Invalid Query:".mysqli_error());
                           while($row=mysqli_fetch_array($res)){
                             echo "<option>".$row['1']."</option>";
