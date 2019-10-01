3dd7b92e8cbc23deeb5b092f41173fb45fe680c4
Omkar <omkar.kakeru@gmail.com>
2019-10-01 16:16:41 +0530

    DB Change

--- DB/inout.sql
+++ DB/inout.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 01, 2019 at 12:38 PM
+-- Generation Time: Oct 01, 2019 at 12:46 PM
 -- Server version: 5.7.25
 -- PHP Version: 7.0.33
 
@@ -44,6 +44,137 @@ CREATE TABLE `inout` (
   `mob` varchar(20) NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loc`
+--
+
+CREATE TABLE `loc` (
+  `id` int(11) NOT NULL,
+  `loc` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `edate` varchar(20) NOT NULL,
+  `nhead` varchar(50) NOT NULL,
+  `nbody` varchar(600) NOT NULL,
+  `nfoot` varchar(50) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `loc` varchar(100) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(10) NOT NULL,
+  `rname` varchar(30) NOT NULL,
+  `rdesc` varchar(100) NOT NULL,
+  `acc_code` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `rname`, `rdesc`, `acc_code`) VALUES
+(1, 'Master', 'Superuser', 'INDEX;S01;A02;R01;N01;R01;'),
+(2, 'User', 'User Dashboard', 'U02;'),
+(3, 'Admin', 'Admin ', 'INDEX;R01;U02;U03;');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `setup`
+--
+
+CREATE TABLE `setup` (
+  `var` varchar(100) DEFAULT NULL,
+  `value` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `setup`
+--
+
+INSERT INTO `setup` (`var`, `value`) VALUES
+('cname', 'In Out Management System'),
+('libtime', '20:30:00'),
+('noname', 'Hello'),
+('sessiontime', '7200');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tmp1`
+--
+
+CREATE TABLE `tmp1` (
+  `date` date NOT NULL,
+  `secs` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tmp2`
+--
+
+CREATE TABLE `tmp2` (
+  `usn` varchar(30) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tmp3`
+--
+
+CREATE TABLE `tmp3` (
+  `date` varchar(30) NOT NULL DEFAULT '',
+  `day` varchar(30) NOT NULL DEFAULT 'CURRENT_TIMESTAMP',
+  `loc` varchar(100) NOT NULL,
+  `category` varchar(100) DEFAULT NULL,
+  `num` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `fname` varchar(30) NOT NULL,
+  `pass` varchar(100) NOT NULL,
+  `role` int(10) NOT NULL,
+  `active` int(2) NOT NULL,
+  `llogin` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `fname`, `pass`, `role`, `active`, `llogin`) VALUES
+(1, 'master', 'Superuser', '8e67bb26b358e2ed20fe552ed6fb832f397a507d', 1, 1, '18/10/2018 09:10 AM'),
+(2, 'admin', 'Administrator', '00299a408dc3498a3cd7bae6db588f3324654d76', 3, 1, '18/10/2018 09:10 AM'),
+(3, 'user', 'User', '7c4a8d09ca3762af61e59520943dc26494f8941b', 2, 1, '07/09/2019 23:09 PM');
+
 --
 -- Indexes for dumped tables
 --
@@ -53,6 +184,42 @@ CREATE TABLE `inout` (
 --
 ALTER TABLE `inout`
   ADD PRIMARY KEY (`sl`);
+
+--
+-- Indexes for table `loc`
+--
+ALTER TABLE `loc`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `rname` (`rname`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `loc`
+--
+ALTER TABLE `loc`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
